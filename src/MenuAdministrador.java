bd0dd204f48004fc8b431206affdde6a2e61e7a8
POO <ariel.ashqui@epn.edu.ec>
2024-08-05 13:52:46 -0500

    Avance proyecto

--- src/MenuAdministrador.java
+++ src/MenuAdministrador.java
@@ -5,9 +5,35 @@ import java.awt.event.ActionListener;
 public class MenuAdministrador extends JFrame{
     private JButton SALIRButton;
     private JRadioButton gestionarCajerosRadioButton;
-    private JRadioButton revisarVentasRadioButton;
-    private JRadioButton gestionarProductosRadioButton;
     private JPanel panel_adminstrador;
+    private JTabbedPane tabbedPane1;
+    private JTabbedPane tabbedPane2;
+    private JTextField textField1;
+    private JButton REGISTRARButton;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JButton BUSCARButton;
+    private JButton ELIMINARButton;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JButton SALIRButton1;
+    private JButton ACTUALIZARButton;
+    private JTabbedPane tabbedPane3;
+    private JTextField textField12;
+    private JTextField textField13;
+    private JTextField textField14;
+    private JTextField textField15;
+    private JTextField textField16;
+    private JTextField textField17;
+    private JTextField textField18;
+    private JButton SALIRButton2;
+    private JButton REGISTRARButton1;
 
     public MenuAdministrador (){
         super("MENU ADMINSTRADOR");
